5d1487601e27f734525d34c5412513e1bf2eec32
Amit Maurya <unknownstudent8765@gmail.com>
2026-08-24 18:23:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,29 @@ const app = express()
 
 app.use(cookieParser())
 
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// )
+
+const allowedOrigins = process.env.CORS_ORIGIN
+  .split(",")
+  .map((origin) => origin.trim());
+
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
-)
+);
 app.get("/", (req, res) => {
     res.status(200).json({
         success: true,
